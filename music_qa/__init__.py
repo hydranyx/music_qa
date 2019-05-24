8f8631dc0ecbf345e7ca3198f6f5886a678e4460
prime <jordis1995@live.nl>
2019-05-24 02:23:54 +0200

    Update doc-comments

--- music_qa/__init__.py
+++ music_qa/__init__.py
@@ -1,3 +1,2 @@
+""" The entry point for the Music QA library. """
 __version__ = "0.1.0"
-
-from .main import main
--- tests/test_music_qa.py
+++ tests/test_music_qa.py
@@ -1,5 +1,7 @@
+""" Integration tests. """
 from music_qa import __version__
 
 
 def test_version():
+    """ Ensure that the version matches expectation. """
     assert __version__ == "0.1.0"
