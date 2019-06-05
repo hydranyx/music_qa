1ebdfb213b30f2bf82bf7cb86e41aded1c51d90f
Boyd Belshof <b.belshof@hotmail.com>
2019-06-05 14:57:26 +0200

    Added Dictionary

--- music_qa/dictionary.py
+++ music_qa/dictionary.py
@@ -1,3 +1,13 @@
-def dictionary(self):
-    birth name = ["real name", "given name"]
-    occupation = ["play on", "job"]
+def get_dictionary():
+	''' Add item to dictionary by using key followed by a list of items that are synonyms
+		EXAMPLE: "birth name": ['real name', 'given name']
+	'''
+    dictionary = {"birth name": ["real name", "given name"],
+     "occupation": ["play on", "job"]}
+
+     return dictionary
+
+def get_dictionary_synonym(prop):
+	dictionary = get_dictionary()
+	return dictionary[prop]
+
