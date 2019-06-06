25ec7a77baa18dbecc37e686d6f7a7e37b33daf0
jeroenvanbrandenburg <jeroen.vb@live.nl>
2019-06-06 16:23:42 +0200

    Added boolean_question

--- music_qa/extract.py
+++ music_qa/extract.py
@@ -31,7 +31,27 @@ def list_question(question):
 
 def boolean_question(question):
 	print(question)
-	return ['prop', 'entity']
+
+	#Getting the individual words, and the dependencies
+    words, dep_list = get_words_and_dep(question)
+
+    #Try to get an entity 
+    ent = get_word_by_dep(words, dep_list, 'nsubj')
+
+    #If there is an entity, try to get an attribute
+    if ent:
+        attr = get_word_by_dep(words, dep_list, 'attr')
+        #If there is no attr, often the verb/root is the attribute
+        if not attr:
+            attr = get_word_by_dep(words, dep_list, 'ROOT')
+
+    #If there is no entity, often the ROOT is the entity and dobj the attribute (e.g. Is rapping a music style?)
+    else:
+        ent = get_word_by_dep(words, dep_list, 'ROOT')
+        attr = get_word_by_dep(words, dep_list, 'dobj')
+	
+	#TODO a boolean question has another pair of words (not a prop, but a attr)
+	return [attr, ent]
 
 def count_question(question):
 	print(question)
@@ -72,6 +92,18 @@ def get_word_by_dep(words, dep_list, dep):
     else:
         print('Failed to retreive a ', dep)
     return result
+
+def get_words_and_dep(question):
+    nlp = spacy.load("en_core_web_sm")
+    nlp.vocab["name"].is_stop = False
+    tokens = []
+    types = []
+    parse = nlp(question.strip())
+    for q in parse:
+        tokens.append(q)
+        types.append(q.dep_)
+    return tokens, types
+pass
 
 if __name__ == "__main__":
 	nlp = spacy.load('en_core_web_sm')
