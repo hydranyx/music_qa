b24c84a320197bd1222e7228a38fee2be26a2f4a
Chiel <chielwijs@live.nl>
2019-06-11 21:48:49 +0200

    updated answer printer for dates

--- music_qa/answer.py
+++ music_qa/answer.py
@@ -1,7 +1,7 @@
 """
 A module containing the Answer intermediary class.
 """
-
+import re
 
 class Answer:
     """
@@ -16,12 +16,20 @@ class Answer:
             return "No answer found."
         elif isinstance(self.answer, list) and len(self.answer) == 1:
             str_ = str(self.answer[0])
+            
+            regex = r"([0-9\-]*)"
+            date = re.search(regex, str_)
+
+            if date.group():
+                str_ = date.group()
+
         elif isinstance(self.answer, bool):
             if self.answer:
                 str_ = "Yes"
             else:
                 str_ = "No"
         elif isinstance(self.answer, list) and len(self.answer) > 1:
+            str_ = ''
             for val in self.answer:
                 str_ = str_ + val + "\t"
                 str_.strip()
